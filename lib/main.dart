7eab8fde231517bdf9c6752f87730c702749a6b3
Darshanadp <darshanadasanayake99@gmail.com>
2024-01-14 04:22:09 +0530

    Major updates are done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:Mobile_CA_2/pages/home.dart';
 import 'package:Mobile_CA_2/pages/testing.dart';
 import 'package:flutter/material.dart';
 
+import 'package:Mobile_CA_2/pages/order.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,6 +14,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Home();
+    return const Order();
   }
 }
--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,24 +1,399 @@
-import 'package:Mobile_CA_2/assets/colors/colors.dart';
-import 'package:flutter/material.dart';
+import 'dart:html';
 
-class Order extends StatefulWidget {
+import 'package:Mobile_CA_2/assets/colors/colors.dart';
+import 'package:Mobile_CA_2/components/custom_btn.dart';
+import 'package:flutter/material.dart';
+// import 'package:Mobile_CA_2/constants.dart';
+// import 'package:Mobile_CA_2/models/cart_item.dart';
+// import 'package:Mobile_CA_2/widgets/cart_tile.dart';
+// import 'package:Mobile_CA_2/widgets/check_out_box.dart';
+
+class Order extends StatelessWidget {
   const Order({super.key});
 
   @override
-  State<Order> createState() => _OrderState();
-}
-
-class _OrderState extends State<Order> {
-  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    double mainLinespace = 10; //Space between Two main topics
+    double subLinespace = 5;  //Space between Two sub topics
+
+    const double maintopicSize = 16; //font size of main topic
+    const double subtopicSize = 12;   //font size of sub topic
+    const double contentSize = 10;   //font size of content
+
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       home: Scaffold(
-        backgroundColor: AppColors.pureWhite,
-        body: SafeArea(
-          child: Column(),
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Order",
+          style: TextStyle(
+            fontFamily: "Bold",
+          ),
+          ),
+          leading: Padding(
+            padding: EdgeInsets.only(left: 5),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.chevron_left),
+            ),
+          ),
         ),
-      ),
+        backgroundColor: AppColors.pureWhite,
+        body: SingleChildScrollView(
+          
+          // child: Container(
+          // width: screenWidth,
+          // height: screenHeight - 56,
+          // color:Colors.blue,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(25),
+                  
+                  child: Column(
+                    
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                
+                
+                
+                      Container(
+                        height: 55,
+                        padding: EdgeInsets.only(left: 5.0),
+                        decoration: BoxDecoration(
+                            color: AppColors.btnGrey,
+                            // color: Colors.black.withOpacity(0.10),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                      color: AppColors.btnBrown,
+                                      borderRadius: BorderRadius.circular(15)),
+                                  child: const Center(
+                                    child: Text(
+                                      "Delivery",
+                                      style: TextStyle(
+                                        
+                                        fontFamily: "Regular",
+                                        fontSize: maintopicSize, color: AppColors.pureWhite),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: InkWell(
+                                onTap: () {},
+                                child: const Center(
+                                  child: Text(
+                                    "Pick UP",
+                                    style: TextStyle(
+                                      fontFamily: "Regular",
+                                      fontSize: maintopicSize,
+                                      color: AppColors.textBlack,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      
+                      
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      
+                      
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Delivery Address",
+                            style:TextStyle(
+                              fontFamily: "Bold",
+                              fontSize: maintopicSize,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          SizedBox(
+                            height: mainLinespace,
+                          ),
+                          const Text(
+                            "J1. Kpg Sutoyo",
+                            style: TextStyle(
+                              fontSize: subtopicSize,
+                              fontFamily: "Bold",
+                            ),
+                          ),
+                          SizedBox(
+                            height: subLinespace,
+                          ),
+                          const Text(
+                            "Kpg.Sutoyo No.620, Bilzen, Tanjungbalai.",
+                            style: TextStyle(
+                              fontFamily: "Regular",
+                              fontSize: contentSize,
+                            ),
+                          ),
+                        ],
+                      ),
+                
+                      
+                      SizedBox(
+                        height: mainLinespace,
+                      ),
+                      
+                      
+                      Container(
+                        height: 1,
+                        width: screenWidth,
+                        decoration: const BoxDecoration(
+                          color: AppColors.bodyTextGrey,
+                          
+                        )
+                      ),
+                      
+                      
+                      
+                      SizedBox(
+                        height: mainLinespace*2,
+                      ),
+                      
+                      
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                              fit:BoxFit.cover,image:AssetImage('lib/assets/images/c_chco.png')
+                            ),
+                            color: AppColors.btnBrown,
+                          ),
+                        ),
+                        
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          // mainAxisAlignment: MainAxisAlignment.end,
+                              
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 10.0),
+                                  child: Text(
+                                    "Cappucino",
+                                    // textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      fontFamily: "Bold",
+                                      fontSize: subtopicSize,
+                                      // backgroundColor: Colors.yellow,
+                                      
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: const Text(
+                                    'with Chocolate',
+                                    style: TextStyle(
+                                      fontFamily: "Regular",
+                                      fontSize: contentSize,
+                                      // backgroundColor: Colors.blue,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                      ],),
+                          
+                          
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      
+                      Container(
+                        height: 3,
+                        width: screenWidth,
+                        decoration: const BoxDecoration(
+                          color: AppColors.bodyTextGrey,
+                          
+                        )
+                      ),
+                
+                
+                      SizedBox(
+                        height: mainLinespace,
+                      ),
+                
+                
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                        const Text(
+                          'Payment Summery',
+                          style: TextStyle(
+                            fontFamily: 'Bold',
+                            fontSize: maintopicSize,
+                
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: mainLinespace,
+                        ),
+
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                          Text(
+                            'Price',
+                            style: TextStyle(
+                              fontFamily: 'Regular',
+                              fontSize: contentSize,
+                            ),
+                          ),
+                          Text(
+                            "\$4.53",
+                            style: TextStyle(
+                              fontFamily: 'Bold',
+                              fontSize: contentSize,
+                            ),
+                          )
+                        ],),
+
+                        SizedBox(
+                          height: subLinespace,
+                        ),
+
+
+
+
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                          Text(
+                            'Delivery Fee',
+                            style: TextStyle(
+                              fontFamily: 'Regular',
+                              fontSize: contentSize,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "\$2.0",
+                                style: TextStyle(
+                                  fontFamily: 'Regular',
+                                  fontSize: contentSize,
+                                  decoration: TextDecoration.lineThrough,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                "\$1.0",
+                                style: TextStyle(
+                                  fontFamily: 'Bold',
+                                  fontSize: contentSize,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],),
+                
+                
+                
+                        
+                      ],),
+                      Container(
+                        height: 1,
+                        width: screenWidth,
+                        decoration: const BoxDecoration(
+                          color: AppColors.bodyTextGrey,
+                          
+                        ),
+                      ),
+
+                      
+
+                        SizedBox(
+                          height: mainLinespace,
+                        ),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Total Payment',
+                            style:TextStyle(
+                              fontFamily: 'Regular',
+                              fontSize: contentSize,
+                            )
+                          ),
+                          Text(
+                            '\$ 5.53',
+                            style: TextStyle(
+                              fontFamily: 'Bold',
+                              fontSize: contentSize,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      
+
+                      SizedBox(
+                        height: mainLinespace*2,
+                      ),
+
+                      Container(
+                        alignment: Alignment.center,
+                        height: 50,
+                        width: screenWidth,
+                        decoration: BoxDecoration(
+                          color: AppColors.btnBrown,
+                          borderRadius: BorderRadius.circular(10),
+                    
+                  ),
+                        child: const Text(
+                          'ORDER',
+                          style: TextStyle(
+                            fontFamily: 'Bold',
+                            fontSize: maintopicSize,
+                            color: AppColors.pureWhite
+                          ),
+                        )
+                ),       
+                    
+                    ],
+                  ),
+                ),
+              
+              ],
+            ),
+          ),
+        ),
+        // ),
+      
     );
   }
 }

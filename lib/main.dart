a02017f78c5d4ebd7c36f0143add889790d88917
Darshanadp <darshanadasanayake99@gmail.com>
2024-01-14 23:26:21 +0530

    Final v1.2
    + Add missing component
    + Chnge stateless to stateful application
    + Chnge Order btn
    + Checking application responsiveness
    + Make increment and decreament work

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:Mobile_CA_2/pages/home.dart';
+import 'package:Mobile_CA_2/pages/order.dart';
 import 'package:Mobile_CA_2/pages/testing.dart';
 import 'package:flutter/material.dart';
 
-import 'package:Mobile_CA_2/pages/order.dart';
 
 void main() {
   runApp(const MyApp());

a1fdbfe9b6bdaaebb64fb48adbd54417aa5bef8b
Sohan Prabhath <soizsinghe@gmail.com>
2024-01-19 00:52:44 +0530

    corrections

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,18 @@
-
-
-
-
 import 'package:Mobile_CA_2/pages/home.dart';
-
-import 'package:Mobile_CA_2/pages/testing.dart';
-
-
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-
-
-
     return const Home();
-
-
   }
 }

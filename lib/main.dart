a694024489217db6c423cdd5a2bc772cb04b8ba5
Darshanadp <darshanadasanayake99@gmail.com>
2024-01-18 10:54:03 +0530

    Revert "Merge branch 'delivery' into orders"
    This reverts commit 6e5f34c1f81e2b02e43383f34d3389685534ba38, reversing
    changes made to 007a0b5685366fc56df63d53faf566bcc0433199.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:Mobile_CA_2/pages/order.dart';
 import 'package:Mobile_CA_2/pages/home.dart';
 import 'package:Mobile_CA_2/pages/detail_Item.dart';
 import 'package:Mobile_CA_2/pages/testing.dart';
-import 'package:Mobile_CA_2/pages/delivery.dart';
 import 'package:flutter/material.dart';
 
 
@@ -16,7 +15,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Home();
-
+    return const Order();
   }
 }
